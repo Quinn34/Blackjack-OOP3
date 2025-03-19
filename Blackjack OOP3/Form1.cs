17b6dba39ec672c324d639fed7fdb4ef99401f4f
Quinn34 <1207398@student.roc-nijmegen.nl>
2025-03-19 08:32:58 +0100

    cards

--- Blackjack OOP3/Form1.cs
+++ Blackjack OOP3/Form1.cs
@@ -6,5 +6,10 @@ namespace Blackjack_OOP3
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+             
+        }
     }
 }
---
+++ Blackjack OOP3/classes/Card.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blackjack_OOP3.classes
+{
+    public enum Suits
+    {
+        CLUBS,
+        DIAMONDS,
+        HEARTS,
+        SPADES
+    }
+
+    public enum FaceValues
+    {
+        ACE = 1,
+        TWO,
+        THREE,
+        FOUR,
+        FIVE,
+        SIX,
+        SEVEN,
+        EIGHT,
+        NINE,
+        TEN,
+        JACK,
+        QUEEN,
+        KING
+    }
+    public class Card
+    {
+        Suits suit;
+        FaceValues faceValue;
+        int value;
+        Image img;
+
+        public Suits Suit
+        {
+            get
+            {
+                return suit;
+            }
+        }
+
+        public int Value
+        {
+            get { return value; }
+            set
+            {
+                if (faceValue == FaceValues.ACE)
+                {
+                    this.value = value;
+                }
+            }
+        }
+
+        public Card(Suits suit, FaceValues faceValue)
+        {
+            this.suit = suit;
+            this.faceValue = faceValue;
+            switch (faceValue)
+            {
+                case FaceValues.ACE:
+                    this.value = 11;
+                    break;
+                case FaceValues.TEN:
+                case FaceValues.JACK:
+                case FaceValues.QUEEN:
+                case FaceValues.KING:
+                    this.value = 10;
+                    break;
+                default:
+                    this.value = (int)faceValue;
+                    break;
+            }
+        }
+
+        public override string ToString()
+        {
+            return this.faceValue + " Of " + this.suit;
+        }
+    }
+}
